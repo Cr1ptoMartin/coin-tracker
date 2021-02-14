3e6e1eeb73b19e8d3ed796004efbdeb883d0e8cc
Cr1iptoMartin <Cr1ptoMartinarg@gmail.com>
2021-02-14 11:38:00 -0300

    Coingecko mas gas tracker files

--- coingecko.js
+++ coingecko.js
@@ -2,36 +2,18 @@ const axios = require('axios');
 var _ = require('lodash');
 var __ =  require('dotenv').config();
 
-const API_URL = process.env.API_URL;
-const API_KEY = process.env.API_KEY;
-const ENTIRE_API_URL = `${API_URL}&apikey=${API_KEY}`;
+const API_URL = process.env.COINGECKO_API_URL+process.env.COINGECKO_PING;
 
-function getGasPrice(){
-	axios.get(ENTIRE_API_URL)
+function getCoingeckoStatus(){
+	axios.get(API_URL)
 		.then(response => {
-			//getting the Minimum gas price, Propose Gas Price and Fast Gas price
-			const minimumGasPrice = response.data.result.SafeGasPrice
-			const proposeGasPrice = response.data.result.ProposeGasPrice
-			const fastGasPrice = response.data.result.FastGasPrice
-
-			//Building final message
-			const message = (
-				`Minimum: ${minimumGasPrice} \n
-				 Propose: ${proposeGasPrice} \n
-				 Fast: ${fastGasPrice} `.replace(/\s+/g, ' ')
-			);
-
-			const targerGasPrice = 200;
-			// Check if it's lower than target gas price
-			if (proposeGasPrice <= targerGasPrice)
-		    		console.log(message);
-
+		    		console.log(response.status + ' ' + response.statusText);
  		})
-		//.then(response => console.log(JSON.stringify(response.data, undefined, 2)))
+
     .catch(error => console.log('Error', error));
 	
-	const intervalTime = 30000;
-	setTimeout(getGasPrice, intervalTime);
+	//const intervalTime = 30000;
+	//setTimeout(getCoingeckoStatus, intervalTime);
 }
 
-getGasPrice();
+getCoingeckoStatus();
---
+++ gastracker.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+var _ = require('lodash');
+var __ =  require('dotenv').config();
+
+const API_URL = process.env.ETHERSCAN_API_URL+process.env.ETHERSCAN_GAS_TRACKER_MODULE+process.env.ETHERSCAN_GAS_TRACKER_ACTION;
+const API_KEY = process.env.API_KEY;
+const ENTIRE_API_URL = `${API_URL}&apikey=${API_KEY}`;
+
+function getGasPrice(){
+	axios.get(ENTIRE_API_URL)
+		.then(response => {
+			//getting the Minimum gas price, Propose Gas Price and Fast Gas price
+			const minimumGasPrice = response.data.result.SafeGasPrice
+			const proposeGasPrice = response.data.result.ProposeGasPrice
+			const fastGasPrice = response.data.result.FastGasPrice
+
+			//Building final message
+			const message = (
+				`Minimum: ${minimumGasPrice} \n
+				 Propose: ${proposeGasPrice} \n
+				 Fast: ${fastGasPrice} `.replace(/\s+/g, ' ')
+			);
+
+			const targerGasPrice = 200;
+			// Check if it's lower than target gas price
+			if (proposeGasPrice <= targerGasPrice)
+		    		console.log(message);
+
+ 		})
+		//.then(response => console.log(JSON.stringify(response.data, undefined, 2)))
+    .catch(error => console.log('Error', error));
+	
+	const intervalTime = 30000;
+	setTimeout(getGasPrice, intervalTime);
+}
+
+getGasPrice();
